dd20dfa03f1b7ec3b58966fae2543055a0b87ea9
Roberto <gleidsonamdd@gmail.com>
2023-04-18 09:05:42 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SomaMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de números a serem lidos (entre 3 e 10):");
+            int qtdNumeros = int.Parse(Console.ReadLine());
+
+            if (qtdNumeros < 3 || qtdNumeros > 10)
+            {
+                Console.WriteLine("Quantidade inválida de números!");
+                return;
+            }
+
+            int soma = 0;
+            for (int i = 1; i <= qtdNumeros; i++)
+            {
+                Console.WriteLine($"Digite o {i}º número:");
+                int numero = int.Parse(Console.ReadLine());
+                soma += numero;
+            }
+
+            double media = (double)soma / qtdNumeros;
+
+            Console.WriteLine($"Soma: {soma}");
+            Console.WriteLine($"Média: {media}");
+        }
+    }
+}
